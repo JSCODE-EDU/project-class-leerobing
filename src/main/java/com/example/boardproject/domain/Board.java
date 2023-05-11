07555fd9363dd8414340efc8cdaf6780ccd66797
leerobing <105533691+leerobing@users.noreply.github.com>
2023-05-12 00:27:31 +0900

    feat: Board 엔티티 생성

---
+++ src/main/java/com/example/boardproject/domain/Board.java
@@ -0,0 +1,32 @@
+package com.example.boardproject.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
+
+
+    @Builder
+    public  Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+
+    }
+
+}
