c63825a210ee2744d3a51c650ead1253e1dbb3d2
leerobing <105533691+leerobing@users.noreply.github.com>
2023-05-22 23:52:57 +0900

    feat: swagger 이용하여 api문서 작성

---
+++ src/main/java/com/example/boardproject/config/swagger/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.example.boardproject.config.swagger;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableWebMvc              // spring-security와 연결할 때 이 부분이 없으면 404에러가 뜬다.
+@EnableSwagger2            // 기능을 활성화하는 어노테이션입니다.
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    /**
+     * Swagger2 버전은 http://localhost:8080/swagger-ui.html
+     * spring-security와 연결할 때 이 부분을 작성하지 않으면 404에러가 뜬다.
+     *
+     * 3.x 버전 부터는 swagger-ui 경로가 다르다고 합니다.
+     * http://localhost:8080/swagger-ui/index.html 로 접근해보세요
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        // -- Static resources
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+
+    }
+}
--- src/main/java/com/example/boardproject/controller/BoardController.java
+++ src/main/java/com/example/boardproject/controller/BoardController.java
@@ -11,6 +11,10 @@ import com.example.boardproject.response.ListResponse;
 import com.example.boardproject.response.ResponseService;
 import com.example.boardproject.response.SingleResponse;
 import com.example.boardproject.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +25,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/board")
 @RequiredArgsConstructor
+@Api(tags = "게시판 컨트롤러")
 public class BoardController {
 
     private final BoardService boardService;
     private final ResponseService responseService;
 
+    @ApiOperation(value = "게시글 작성 기능", notes = "제목, 내용을 입력해 게시글을 작성한다.")
     @PostMapping("/write")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "게시글 작성 성공."),
+            @ApiResponse(code = 400, message = "제목 또는 내용이 유효성 검사에서 실패할 때.", response = String.class)
+    })
     public SingleResponse<Board> boardWrite(@RequestBody @Valid SaveRequestDto saveRequestDto) {
         Board board = boardService.write(saveRequestDto);
         return responseService.getSingleResponse(board);
