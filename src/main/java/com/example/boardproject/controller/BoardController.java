e872b87dac980b2c6f8166b5367f08baca8b0b24
leerobing <105533691+leerobing@users.noreply.github.com>
2023-05-23 13:49:22 +0900

    refactor: api문서 수정

--- src/main/java/com/example/boardproject/controller/BoardController.java
+++ src/main/java/com/example/boardproject/controller/BoardController.java
@@ -32,33 +32,60 @@ public class BoardController {
     private final ResponseService responseService;
 
     @ApiOperation(value = "게시글 작성 기능", notes = "제목, 내용을 입력해 게시글을 작성한다.")
-    @PostMapping("/write")
     @ResponseStatus(HttpStatus.CREATED)
     @ApiResponses({
             @ApiResponse(code = 201, message = "게시글 작성 성공."),
             @ApiResponse(code = 400, message = "제목 또는 내용이 유효성 검사에서 실패할 때.", response = String.class)
     })
+    @PostMapping("/write")
     public SingleResponse<Board> boardWrite(@RequestBody @Valid SaveRequestDto saveRequestDto) {
         Board board = boardService.write(saveRequestDto);
         return responseService.getSingleResponse(board);
 
     }
+    @ApiOperation(value = "게시글 조회 기능", notes = "특정 게시글만 조회하는 api")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 404, message = "존재하지 않는 게시글입니다."),
+            @ApiResponse(code = 500, message = "서버 내부 오류입니다 관리자에게 문의 부탁드립니다.")
+
+    })
+    @ResponseStatus(HttpStatus.CREATED)
     @GetMapping("/{id}")
     public SingleResponse<Board> boardRead(@PathVariable Long id) throws InvalidateBoardException {
         return responseService.getSingleResponse(boardService.find(id));
     }
 
+    @ApiOperation(value = "게시글 전체 조회 기능", notes = "전체 게시글을 조회하는 api")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 404, message = "존재하지 않는 게시글입니다."),
+            @ApiResponse(code = 500, message = "서버 내부 오류입니다, 관리자에게 문의 부탁드립니다.")
+    })
     @GetMapping("/find-all")
     public ListResponse<Board> boardReadAll() {
         return responseService.getListResponse(boardService.findAll());
     }
 
     @GetMapping()
+    @ApiOperation(value = "게시글 검색",notes = "검색 조건에 맞는 게시글을 조회하는 api")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 400, message = "검색 키워드는 공백을 제외한 1글자 이상이어야 합니다."),
+            @ApiResponse(code = 404, message = "존재하지 않는 게시글입니다."),
+            @ApiResponse(code = 500, message = "서버 내부 오류입니다. 관리자에게 문의해주시기 바랍니다.")
+    })
     public ListResponse<Board> search(@RequestParam("keyword") String keyword) {
         return responseService.getListResponse(boardService.searchFind(keyword));
     }
 
     @PatchMapping("/{id}")
+    @ApiOperation(value = "특정 게시글 수정", notes = "특정 id의 게시글을 수정하는 API")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 404, message = "존재하지 않는 게시글입니다."),
+            @ApiResponse(code = 500, message = "서버 내부 오류입니다. 관리자에게 문의해주시기 바랍니다.")
+    })
     public SingleResponse boardModify(@PathVariable("id") Long id,
                                         @RequestBody @Valid ModifyRequestDto modifyRequestDto) throws InvalidateBoardException {
         Board board = boardService.modify(id,modifyRequestDto);
@@ -66,6 +93,12 @@ public class BoardController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "특정 게시글 삭제", notes = "특정 id의 게시글을 삭제하는 API")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "성공"),
+            @ApiResponse(code = 404, message = "존재하지 않는 게시글입니다."),
+            @ApiResponse(code = 500, message = "서버 내부 오류입니다. 관리자에게 문의해주시기 바랍니다.")
+    })
     public String boardDelete(@PathVariable("id") Long id) throws InvalidateBoardException {
         boardService.delete(id);
         return "ok";
--- src/main/java/com/example/boardproject/controller/MemberController.java
+++ src/main/java/com/example/boardproject/controller/MemberController.java
@@ -6,6 +6,10 @@ import com.example.boardproject.config.security.JwtTokenProvider;
 import com.example.boardproject.domain.Member;
 import com.example.boardproject.domain.Role;
 import com.example.boardproject.repository.MemberRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/board")
+@Api(tags = "회원가입 및 로그인 컨트롤러")
 public class MemberController {
 
     private final PasswordEncoder passwordEncoder;
@@ -24,6 +29,12 @@ public class MemberController {
     private final MemberRepository memberRepository;
 
     // 회원가입
+    @ApiOperation(value = "회원가입 기능", notes = "회원가입을 당당하는 컨트롤러")
+    @ApiResponses({
+            @ApiResponse(code = 200,message = "회원가입 성공"),
+            @ApiResponse(code = 404,message = "회원가입 실패"),
+            @ApiResponse(code = 500,message = "서버 내부 오류입니다. 관리자에게 문의바랍니다.")
+    })
     @PostMapping("/join")
     public UUID register(@RequestBody Map<String, String> user) {
         return memberRepository.save(Member.builder()
@@ -36,6 +47,12 @@ public class MemberController {
     }
 
     // 로그인
+    @ApiOperation(value = "로그인 기능", notes = "로그인을 당당하는 컨트롤러")
+    @ApiResponses({
+            @ApiResponse(code = 200,message = "로그인 성공"),
+            @ApiResponse(code = 404,message = "로그인 실패"),
+            @ApiResponse(code = 500,message = "서버 내부 오류입니다. 관리자에게 문의바랍니다.")
+    })
     @PostMapping("/login")
     public String login(@RequestBody Map<String, String> user) {
         Member member = memberRepository.findByEmail(user.get("email"))
